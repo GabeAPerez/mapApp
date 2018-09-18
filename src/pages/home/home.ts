268f7a652bde35c54f7e12755bc94639f3500031
GabeAPerez <GabeAP@stanford.edu>
2018-09-18 01:17:40 -0400

    Ionic Dev Iteration 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation'
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
+
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +20,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+ 
+  map: any;
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
 
+  //Main method
+  ionViewDidLoad(){
+    this.showMap();
+  }  
+
+  //Shows map on the screen
+  showMap(){
+    let latLng = new google.maps.LatLng(0,0);
+    
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 20,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      
+      
+    }, (err) => {
+      console.log(err);
+    });
+
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+        
+ 
+  }
+
+  
 }
